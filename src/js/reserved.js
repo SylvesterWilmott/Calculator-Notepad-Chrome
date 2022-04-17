efe5d3078a0551b2a6d4ab47e4328eee91ed1e32
Sylvester Wilmott <sylvester.wilmott@gmail.com>
2022-04-17 13:00:36 +0100

    Create reserved.js

---
+++ src/js/reserved.js
@@ -0,0 +1,3 @@
+'use strict';
+
+export let identifiers = ['Mod', 'C', 'P', 'Sigma', 'Pi', 'n', 'root', 'pow', 'log', 'ln', 'sin', 'cos', 'tan', 'sinh', 'cosh', 'tanh', 'asin', 'acos', 'atan', 'asinh', 'acosh', 'atanh'];
