e425c74d733c8e0d5ae7a84765146e96876850a0
Sylvester Wilmott <sylvester.wilmott@gmail.com>
2022-05-17 13:16:50 +0100

    Update regex.js

--- src/js/regex.js
+++ src/js/regex.js
@@ -5,3 +5,4 @@ export const headingRegex = /^(.*?):$/gm;
 export const variableRegex = /^\s*([a-zA-Z_]+) ?(\bis\b|=) ?([^=]+)$/gm;
 export const wordRegex = /[a-z_]+/gi;
 export const numberSuffixRegex = /(\d+(?:\.\d+)?)([KkMB]{1}\b)/g;
+export const tabRegex = /\t/g;
